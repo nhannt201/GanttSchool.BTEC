76308f580c43e144c4b13f4c5ba0536ced0ec4a5
nhannt201 <trungnhan21.12@gmail.com>
2020-08-11 09:13:02 +0700

    Change type POST data

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 require_once("inc/config.php");
 $login = new Login();
 if (isset($_POST['teacher_log'])) {
-	$user = $_POST['user'];
-	$pass = $_POST['pass'];
+	$user = isset($_POST['user']) ? $_POST['user'] : '';
+	$pass = isset($_POST['pass']) ? $_POST['pass'] : '';
 	$login->checklog(1, $user, $pass);
 }
 if (isset($_POST['student_log'])) {
-	$user = $_POST['user'];
-	$pass = $_POST['pass'];
+	$user = isset($_POST['user']) ? $_POST['user'] : '';
+	$pass = isset($_POST['pass']) ? $_POST['pass'] : '';
 	$login->checklog(2, $user, $pass);
 }
 if (isset($_POST['parent_log'])) {
-	$user = $_POST['user'];
-	$pass = $_POST['pass'];
+	$user = isset($_POST['user']) ? $_POST['user'] : '';
+	$pass = isset($_POST['pass']) ? $_POST['pass'] : '';
 	$login->checklog(3, $user, $pass);
 }
 ?>
